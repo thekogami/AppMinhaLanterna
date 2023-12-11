75f8138efa84668eeb1ca87466f95e83812eb50d
Felipe <85915186+felipricardo@users.noreply.github.com>
2023-12-11 17:10:06 -0300

    Update MainPage.xaml.cs

--- AppMinhaLanterna/MainPage.xaml.cs
+++ AppMinhaLanterna/MainPage.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 
@@ -11,55 +6,56 @@ namespace AppMinhaLanterna
 {
     public partial class MainPage : ContentPage
     {
+        public bool Ligado { get; set; }
+
+        public void SetLigadoImage(string imageSource)
+        {
+            this.btLigar.Source = imageSource;
+        }
+
         public MainPage()
         {
             InitializeComponent();
+            Ligado = false;
         }
 
         private void btLigar_Clicked(object sender, EventArgs e)
         {
             ImageButton bt = (ImageButton)sender;
             String texto = bt.Source.ToString();
-            if(texto == "File: desligado")
+            if (texto == "File: desligado.jpg")
             {
-                bt.Source = "ligado";
+                bt.Source = "ligado.png";
                 this.LigarCameraAsync(true);
             }
             else
             {
-                bt.Source = "desligado";
+                bt.Source = "desligado.jpg";
                 this.LigarCameraAsync(false);
             }
+
+            Ligado = !Ligado;
         }
 
         private async void LigarCameraAsync(Boolean ligar)
         {
             try
             {
-                if (ligar == true)
-                {
-                    // Turn On
-                    await Flashlight.TurnOnAsync();
-                }
+                if (ligar)
+                    Flashlight.TurnOnAsync(); // Turn On await 
                 else
-                {
-                    // Turn Off
-                    await Flashlight.TurnOffAsync();
-                }               
+                    await Flashlight.TurnOffAsync(); // Turn Off
             }
             catch (FeatureNotSupportedException fnsEx)
             {
-                // Handle not supported on device exception
                 //await DisplayAlert("Erro", fnsEx.Message, "OK");
             }
             catch (PermissionException pEx)
             {
-                // Handle permission exception
                 //await DisplayAlert("Erro", pEx.Message, "OK");
             }
             catch (Exception ex)
             {
-                // Unable to turn on/off flashlight
                 //await DisplayAlert("Erro", ex.Message, "OK");
             }
         }
